6319c241ab202b62d73ebbe1a875bd872aabfead
Bowie <leonardnoliveira@gmail.com>
2023-12-06 13:25:00 -0300

    Initial Commit

---
+++ Travel Destinations App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel Destinations App
+//
+//  Created by Leonard Nucci de Oliveira on 06/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
