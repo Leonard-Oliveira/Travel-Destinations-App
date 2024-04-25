54ae236483a0e67285562e129e2f2159aeb358eb
Bowie <leonardnoliveira@gmail.com>
2024-04-25 20:06:25 -0300

    ViagemCell

---
+++ Travel Destinations App/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  Travel Destinations App
+//
+//  Created by Leonard Nucci de Oliveira on 25/04/24.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
