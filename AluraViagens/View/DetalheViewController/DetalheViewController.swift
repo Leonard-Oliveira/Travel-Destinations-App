ab2d1876c6546950c87c7b0bd415e1a8251aedd9
Bowie <leonardnoliveira@gmail.com>
2024-04-30 21:29:13 -0300

    Views

---
+++ AluraViagens/View/DetalheViewController/DetalheViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetalheViewController.swift
+//  AluraViagens
+//
+//  Created by Leonard Nucci de Oliveira on 30/04/24.
+//
+
+import UIKit
+
+class DetalheViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+ 
+
+}
--- AluraViagens/View/ViewController.swift
+++ AluraViagens/View/ViewController.swift
@@ -69,9 +69,11 @@ extension ViewController: UITableViewDataSource {
 extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let detalheController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DetalheViewController")
+        let detalheController = DetalheViewController(nibName: "DetalheViewController", bundle: nil)
         navigationController?.pushViewController(detalheController, animated: true)
     }
+    
+    
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if section == 0 {
