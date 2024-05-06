4311f149436e64626fb07304f0dee3b45126af00
Bowie <leonardnoliveira@gmail.com>
2024-05-05 21:38:17 -0300

    scroll view v1

--- AluraViagens/View/DetalheViewController/DetalheViewController.swift
+++ AluraViagens/View/DetalheViewController/DetalheViewController.swift
@@ -2,7 +2,7 @@
 //  DetalheViewController.swift
 //  AluraViagens
 //
-//  Created by Leonard Nucci de Oliveira on 30/04/24.
+//  Created by Leonard Nucci de Oliveira on 05/05/24.
 //
 
 import UIKit
@@ -12,8 +12,18 @@ class DetalheViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
     }
 
- 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
